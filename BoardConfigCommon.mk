7fa071915103a55a264fbbc03c94d6522cbaabe4
Steve Kondik <shade@chemlab.org>
2013-04-30 20:10:28 -0700

    Initial import

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,74 @@
+# Copyright (C) 2009 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file sets variables that control the way modules are built
+# thorughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
+#
+
+# WARNING: This line must come *before* including the proprietary
+# variant, so that it gets overwritten by the parent (which goes
+# against the traditional rules of inheritance).
+USE_CAMERA_STUB := true
+
+# inherit from common msm8960
+-include device/samsung/msm8960-common/BoardConfigCommon.mk
+
+# Kernel
+TARGET_KERNEL_SOURCE        := kernel/samsung/jf
+BOARD_KERNEL_CMDLINE        := androidboot.hardware=qcom user_debug=31 zcache
+BOARD_KERNEL_BASE           := 0x80200000
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
+BOARD_KERNEL_PAGESIZE       := 2048
+
+TARGET_BOOTLOADER_BOARD_NAME := MSM8960
+
+# Recovery
+BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/jf-common/recovery/recovery_keys.c
+BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_15x24.h\"
+BOARD_USES_MMCUTILS := true
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_HAS_NO_MISC_PARTITION := true
+BOARD_HAS_NO_SELECT_BUTTON := true
+TARGET_RECOVERY_FSTAB := device/samsung/jf-common/recovery.fstab
+
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00A00000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00A00000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1572864000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 28651290624
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+# bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/jf-common/bluetooth
+BOARD_BLUEDROID_VENDOR_CONF := device/samsung/jf-common/bluetooth/vnd_jf.txt
+BOARD_BLUETOOTH_USES_HCIATTACH_PROPERTY := false
+
+# Disable initlogo, Samsungs framebuffer is weird
+TARGET_NO_INITLOGO := true
+
+# HAX
+#BOARD_USE_SAMSUNG_SEPARATEDSTREAM := true
+#BOARD_USES_LIBMEDIA_WITH_AUDIOPARAMETER := true
+#TARGET_PROVIDES_LIBAUDIO := true
+
+# Use Audience A2220 chip
+BOARD_HAVE_AUDIENCE_A2220 := true
+
+# Use USB Dock Audio
+BOARD_HAVE_DOCK_USBAUDIO := true
+
---
+++ jf-common.mk
@@ -0,0 +1,128 @@
+#
+# Copyright (C) 2011 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+## (2) Also get non-open-source specific aspects if available
+$(call inherit-product-if-exists, vendor/samsung/jf-common/jf-common-vendor.mk)
+
+## overlays
+DEVICE_PACKAGE_OVERLAYS += device/samsung/jf-common/overlay
+
+# Device uses high-density artwork where available
+PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
+# Audio configuration
+PRODUCT_COPY_FILES += \
+        device/samsung/jf-common/audio/snd_soc_msm_2x:system/etc/snd_soc_msm/snd_soc_msm_2x \
+        device/samsung/jf-common/audio/audio_policy.conf:system/etc/audio_policy.conf \
+        device/samsung/jf-common/audio/audio_effects.conf:system/etc/audio_effects.conf
+
+# Ramdisk
+PRODUCT_PACKAGES += \
+    fstab.qcom \
+    initlogo.rle \
+    init.bt.rc \
+    init.qcom.rc \
+    init.qcom.usb.rc \
+    init.target.rc \
+    ueventd.qcom.rc
+
+# Torch
+PRODUCT_PACKAGES += Torch
+
+# Vold configuration
+PRODUCT_COPY_FILES += \
+    device/samsung/jf-common/vold.fstab:system/etc/vold.fstab
+
+# Wifi
+PRODUCT_PACKAGES += \
+    libnetcmdiface
+
+# Set default USB interface
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp
+
+# Lights
+PRODUCT_PACKAGES += lights.msm8960
+
+# Increase the HWUI font cache since we have tons of RAM
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hwui.text_cache_width=2048
+
+# QC Perf
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.extension_library=/system/lib/libqc-opt.so
+
+# QRNGD
+PRODUCT_PACKAGES += qrngd
+
+#common build.props
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.chipname=apq8064 \
+    ro.ril.hsxpa=1 \
+    ro.ril.gprsclass=10 \
+    persist.radio.add_power_save=1 \
+    persist.radio.snapshot_disabled=1 \
+    persist.radio.use_se_table_only=1 \
+    persist.radio.fill_eons=1 \
+    com.qc.hardware=true \
+    persist.radio.apm_sim_not_pwdn=1 \
+    ro.telephony.call_ring.multiple=0 \
+    ro.sf.lcd_density=320 \
+    ro.ril.transmitpower=true \
+    ro.opengles.version=131072 \
+    persist.audio.fluence.mode=endfire \
+    persist.audio.vr.enable=false \
+    persist.audio.handset.mic=digital \
+    persist.audio.speaker.location=high \
+    ro.qc.sdk.audio.fluencetype=fluence \
+    persist.timed.enable=true \
+    ro.emmc.sdcard.partition=17 \
+    ro.use_data_netmgrd=true \
+    persist.data.ds_fmc_app.mode=0 \
+    persist.ims.regmanager.mode=0 \
+    ro.config.ehrpd=true \
+    persist.omh.enabled=1 \
+    persist.data_netmgrd_nint=16 \
+    lpa.decode=true \
+    rild.libpath=/system/lib/libril-qc-qmi-1.so \
+    ril.subscription.types=NV,RUIM \
+    ro.config.svlte1x=true \
+    ro.cdma.subscribe_on_ruim_ready=true \
+    persist.radio.no_wait_for_card=0 \
+    keyguard.no_require_sim=true \
+    media.aac_51_output_enabled=true \
+    persist.rild.nitz_plmn="" \
+    persist.rild.nitz_long_ons_0="" \
+    persist.rild.nitz_long_ons_1="" \
+    persist.rild.nitz_long_ons_2="" \
+    persist.rild.nitz_long_ons_3="" \
+    persist.rild.nitz_short_ons_0="" \
+    persist.rild.nitz_short_ons_1="" \
+    persist.rild.nitz_short_ons_2="" \
+    persist.rild.nitz_short_ons_3=""
+
+# common msm8960
+$(call inherit-product, device/samsung/msm8960-common/msm8960.mk)
+
+$(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
+
