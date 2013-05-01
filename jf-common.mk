8925af620655ff6d226d22a81f2b07b4f2f14ef9
Steve Kondik <shade@chemlab.org>
2013-05-01 07:32:05 -0700

    jf: Add keylayouts

--- jf-common.mk
+++ jf-common.mk
@@ -43,6 +43,16 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     device/samsung/msm8960-common/gps/gps.conf:system/etc/gps.conf
 
+# Keylayouts
+PRODUCT_COPY_FILES += \
+    device/samsung/jf-common/keylayout/atmel_mxt_ts.kl:system/usr/keylayout/atmel_mxt_ts.kl \
+    device/samsung/jf-common/keylayout/Button_Jack.kl:system/usr/keylayout/Button_Jack.kl \
+    device/samsung/jf-common/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
+    device/samsung/jf-common/keylayout/philips_remote_ir.kl:system/usr/keylayout/philips_remote_ir.kl \
+    device/samsung/jf-common/keylayout/samsung_remote_ir.kl:system/usr/keylayout/samsung_remote_ir.kl \
+    device/samsung/jf-common/keylayout/sec_touchkey.kl:system/usr/keylayout/sec_touchkey.kl \
+    device/samsung/jf-common/keylayout/ue_rf4ce_remote.kl:system/usr/keylayout/ue_rf4ce_remote.kl
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.qcom \
