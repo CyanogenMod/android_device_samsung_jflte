bd28b5369e926fcbe2994789f0bc781d46ac4a40
Steve Kondik <shade@chemlab.org>
2013-07-05 14:22:35 -0700

    jf: Fix BCM4335 chip crashes caused by BT/WiFI concurrency
     * The stock B+ driver works around chip crash that occurs when WiFi is
       on and BT is enabled/disabled by sending a magic patch to the
       hardware.
     * This patch was not in an external file, so some reverse engineering
       was required to extract it and put it in the correct format.
     * Enable the prepatch and enjoy crash-free Bluetooth :)
    
    Change-Id: I1ec8ec345c459ce97cc389d270b025ef61363018

--- jf-common.mk
+++ jf-common.mk
@@ -119,6 +119,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # QRNGD
 PRODUCT_PACKAGES += qrngd
 
+# Prepatch to fix BT/WiFi bus lockups
+PRODUCT_COPY_FILES += device/samsung/jf-common/bluetooth/bcm4335_prepatch.hcd:system/vendor/firmware/bcm4335_prepatch.hcd
+
 #common build.props
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.chipname=apq8064 \
