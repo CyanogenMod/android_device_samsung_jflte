7c19f3339b0e772896661a187d8d1f984ad362f5
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-08-14 02:38:50 -0400

    Expose Irda feature
    Patch Set 2: Add missing backslash
    
    Patch Set 4: PS3 reverted PS2
    
    Change-Id: I2a3b2aabb92b1d0c86ec0974a4b939eaba689879

--- jf-common.mk
+++ jf-common.mk
@@ -30,6 +30,10 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
+# Expose Irda feature
+PRODUCT_COPY_FILES += \
+    device/samsung/jf-common/etc/com.sec.feature.irda_service.xml:system/etc/permissions/com.sec.feature.irda_service.xml
+
 # Audio configuration
 PRODUCT_COPY_FILES += \
     device/samsung/jf-common/audio/snd_soc_msm_2x_Fusion3:system/etc/snd_soc_msm/snd_soc_msm_2x_Fusion3 \
