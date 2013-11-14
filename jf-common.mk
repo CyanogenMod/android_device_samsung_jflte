7d1951960ea4e5129aa18dd559b487139ec6e122
Dan Pasanen <dan.pasanen@gmail.com>
2013-11-13 22:36:53 -0600

    jf: enable repeat keys in cwm
    Change-Id: I26bf2a7f6e8846ad9426c6af5a984082cf8ece81

--- jf-common.mk
+++ jf-common.mk
@@ -130,6 +130,11 @@ PRODUCT_PACKAGES += qrngd
 # Prepatch to fix BT/WiFi bus lockups
 PRODUCT_COPY_FILES += device/samsung/jf-common/bluetooth/bcm4335_prepatch.hcd:system/vendor/firmware/bcm4335_prepatch.hcd
 
+# enable repeatable keys in cwm
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.cwm.enable_key_repeat=true \
+    ro.cwm.repeatable_keys=114,115
+
 #common build.props
 PRODUCT_PROPERTY_OVERRIDES += \
     wifi.interface=wlan0 \
