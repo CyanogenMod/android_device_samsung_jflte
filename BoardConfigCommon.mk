b867b9ef77ea1cc883e8c17a41bf64ba49e72284
Dan Pasanen <dan.pasanen@gmail.com>
2013-12-06 14:09:12 -0600

    jf: moving camera wrapper to msm8960-common
    Change-Id: Iefd66e20eb46d9f5298181a9dd8b60e7407a5db8

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -64,9 +64,6 @@ BOARD_BLUETOOTH_USES_HCIATTACH_PROPERTY := false
 # Needed for blobs
 COMMON_GLOBAL_CFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS
 
-# Don't use qcom camera HAL
-USE_DEVICE_SPECIFIC_CAMERA := true
-
 # Adreno
 OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
 HAVE_ADRENO_SOURCE := false
