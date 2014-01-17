f2d1f7eb8b7292eb026784c9497d3c17fe84d126
Dave Daynard <nardholio@gmail.com>
2014-01-17 16:55:12 -0500

    jf-common: actually build the crda service
    Change-Id: I92ba18b5406c73897444d1db378c23d8561cc147

--- jf-common.mk
+++ jf-common.mk
@@ -74,7 +74,7 @@ PRODUCT_PACKAGES += \
     initlogo.rle \
     init.bt.rc \
     init.carrier.rc \
-    init.crda.rc \
+    init.crda.sh \
     init.qcom.rc \
     init.qcom.usb.rc \
     init.target.rc \
