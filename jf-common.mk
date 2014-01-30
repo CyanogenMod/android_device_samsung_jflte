1dc9df8becb258863be61b6e7afa88320ba5e1be
Dan Pasanen <dan.pasanen@gmail.com>
2014-01-30 10:29:21 -0600

    jf-common: dex-opt to cache
    cache partition is 2gb, seriously, this is fine for us
    
    Change-Id: I82be27c0bc5ea0f93b2ebd9b59f3e7dc0bd62006

--- jf-common.mk
+++ jf-common.mk
@@ -188,7 +188,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.rild.nitz_short_ons_1="" \
     persist.rild.nitz_short_ons_2="" \
     persist.rild.nitz_short_ons_3="" \
-    ro.telephony.ril.v3=newDriverCall
+    ro.telephony.ril.v3=newDriverCall \
+    dalvik.vm.dexopt-data-only=0
 
 # call common msm8960
 $(call inherit-product, device/samsung/msm8960-common/msm8960.mk)
