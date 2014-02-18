d27c860083f264eb398d47526ac91a1d01e80065
Dan Pasanen <dan.pasanen@gmail.com>
2014-02-17 19:12:40 -0600

    jflte: Fix LTE on GSM devices
    Change-Id: Iafff0b439e4cc996557cac5833dff9f7e3d5645f

--- init/init_jflte.c
+++ init/init_jflte.c
@@ -60,12 +60,16 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
         property_set("ro.build.description", "jfltevl-user 4.2.2 JDQ39 I337MVLUAMDJ release-keys");
         property_set("ro.product.model", "SGH-I337M");
         property_set("ro.product.device", "jfltecan");
+        property_set("telephony.lteOnGsmDevice", "1");
+        property_set("ro.telephony.default_network", "9");
     } else if (strstr(bootloader, "I337")) {
         /* jflteatt */
         property_set("ro.product.device", "jflteatt");
         property_set("ro.product.model", "SGH-I337");
         property_set("ro.build.fingerprint", "samsung/jflteuc/jflteatt:4.2.2/JDQ39/I337UCUAMDL");
         property_set("ro.build.description", "jflteuc-user 4.2.2 JDQ39 I337UCUAMDL release-keys");
+        property_set("telephony.lteOnGsmDevice", "1");
+        property_set("ro.telephony.default_network", "9");
     } else if (strstr(bootloader, "R970C")) {
         /* jfltecri */
         property_set("ro.build.fingerprint", "samsung/jfltecri/jfltecri:4.2.2/JDQ39/R970CVVUAME4:user/release-keys");
@@ -73,6 +77,8 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
         property_set("ro.product.model", "SCH-R970C");
         property_set("ro.product.device", "jfltecri");
         property_set("ro.telephony.ril.v3", "newDriverCallU");
+        property_set("telephony.lteOnCdmaDevice", "1");
+        property_set("ro.telephony.default_network", "8");
     } else if (strstr(bootloader, "R970X")) {
         /* jfltecsp */
         property_set("ro.build.fingerprint", "samsung/jfltecsp/jfltecsp:4.2.2/JDQ39/R970XWWUAMFA:user/release-keys");
@@ -80,6 +86,9 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
         property_set("ro.product.model", "SCH-R970X");
         property_set("ro.product.device", "jfltecsp");
         property_set("ro.telephony.ril.v3", "newDriverCallU");
+        property_set("telephony.lteOnCdmaDevice", "1");
+        property_set("ro.telephony.default_network", "8");
+        property_set("telephony.sms.pseudo_multipart", "1");
     } else if (strstr(bootloader, "L720")) {
         /* jfltespr */
         property_set("ro.build.fingerprint", "samsung/jfltespr/jfltespr:4.2.2/JDQ39/L720VPUAMDL:user/release-keys");
@@ -87,12 +96,17 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
         property_set("ro.product.model", "SPH-L720");
         property_set("ro.product.device", "jfltespr");
         property_set("ro.telephony.ril.v3", "newDriverCallU");
+        property_set("telephony.lteOnCdmaDevice", "1");
+        property_set("ro.telephony.default_network", "8");
+        property_set("telephony.sms.pseudo_multipart", "1");
     } else if (strstr(bootloader, "M919")) {
         /* jfltetmo */
         property_set("ro.build.fingerprint", "samsung/jfltetmo/jfltetmo:4.3/JSS15J/M919UVUEMK2:user/release-keys");
         property_set("ro.build.description", "jfltetmo-user 4.3 JSS15J M919UVUEMK2 release-keys");
         property_set("ro.product.model", "SGH-M919");
         property_set("ro.product.device", "jfltetmo");
+        property_set("telephony.lteOnGsmDevice", "1");
+        property_set("ro.telephony.default_network", "9");
     } else if (strstr(bootloader, "R970")) {
         /* jflteusc */
         property_set("ro.build.fingerprint", "samsung/jflteusc/jflteusc:4.2.2/JDQ39/R970VXUAMD9:user/release-keys");
@@ -100,6 +114,9 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
         property_set("ro.product.model", "SCH-R970");
         property_set("ro.product.device", "jflteusc");
         property_set("ro.telephony.ril.v3", "newDriverCallU");
+        property_set("telephony.lteOnCdmaDevice", "1");
+        property_set("ro.telephony.default_network", "8");
+        property_set("telephony.sms.pseudo_multipart", "1");
     } else if (strstr(bootloader, "I545")) {
         /* jfltevzw */
         property_set("ro.build.fingerprint", "Verizon/jfltevzw/jfltevzw:4.3/JSS15J/I545VRUEMK2:user/release-keys");
@@ -110,24 +127,32 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
         property_set("ro.telephony.ril.v3", "newDriverCallU");
         property_set("ro.cdma.home.operator.alpha", "Verizon");
         property_set("ro.cdma.home.operator.numeric", "310004");
+        property_set("telephony.lteOnCdmaDevice", "1");
+        property_set("ro.telephony.default_network", "10");
     } else if (strstr(bootloader, "I9505G")) {
         /* jgedlte */
         property_set("ro.build.fingerprint", "samsung/jgedlteue/jgedlte:4.4.2/KOT49H.S001/131204:user/release-keys");
         property_set("ro.build.description", "jgedlteue-user 4.4.2 KOT49H.S001 131204 release-keys");
         property_set("ro.product.model", "GT-I9505G");
         property_set("ro.product.device", "jgedlte");
+        property_set("telephony.lteOnGsmDevice", "1");
+        property_set("ro.telephony.default_network", "9");
     } else if (strstr(bootloader, "I9505")) {
         /* jfltexx */
         property_set("ro.build.fingerprint", "samsung/jfltexx/jflte:4.2.2/JDQ39/I9505XXUAMDE:user/release-keys");
         property_set("ro.build.description", "jfltexx-user 4.2.2 JDQ39 I9505XXUAMDE release-keys");
         property_set("ro.product.model", "GT-I9505");
         property_set("ro.product.device", "jfltexx");
+        property_set("telephony.lteOnGsmDevice", "1");
+        property_set("ro.telephony.default_network", "9");
     } else if (strstr(bootloader, "I9508")) {
         /* jfltezm */
         property_set("ro.build.fingerprint", "samsung/jfltezm/jflte:4.2.2/JDQ39/I9508ZMUAMG1:user/release-keys");
         property_set("ro.build.description", "jfltezm-user 4.2.2 JDQ39 I9508ZMUAMG1 release-keys");
         property_set("ro.product.model", "GT-I9508");
         property_set("ro.product.device", "jfltezm");
+        property_set("telephony.lteOnGsmDevice", "1");
+        property_set("ro.telephony.default_network", "9");
     }
 
     property_get("ro.product.device", device);
