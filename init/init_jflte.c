2314b4633464cc80f3a77f579aa70f8100b0540d
Markus Guidry <markus@gu1dry.com>
2014-02-28 19:17:54 -0500

    jflteatt: Update init to I337UCUFNB1 (4.4.2)
    This also aligns the ro.product.model with stock.
    
    Change-Id: Ib783eaa3a4bc052704efeae99e0bc45fbc2aa1ab

--- init/init_jflte.c
+++ init/init_jflte.c
@@ -65,9 +65,9 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
     } else if (strstr(bootloader, "I337")) {
         /* jflteatt */
         property_set("ro.product.device", "jflteatt");
-        property_set("ro.product.model", "SGH-I337");
-        property_set("ro.build.fingerprint", "samsung/jflteuc/jflteatt:4.2.2/JDQ39/I337UCUAMDL");
-        property_set("ro.build.description", "jflteuc-user 4.2.2 JDQ39 I337UCUAMDL release-keys");
+        property_set("ro.product.model", "SAMSUNG-SGH-I337");
+        property_set("ro.build.fingerprint", "samsung/jflteuc/jflteatt:4.4.2/KOT49H/I337UCUFNB1:user/release-keys");
+        property_set("ro.build.description", "jflteuc-user 4.4.2 KOT49H I337UCUFNB1 release-keys");
         property_set("telephony.lteOnGsmDevice", "1");
         property_set("ro.telephony.default_network", "9");
         property_set("ro.loki_enabled", "1");
