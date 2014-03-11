0eb5ccb755f4c7955bc921dfd4ba092a9aa11d40
nuclearmistake <nuclearmistake@gmail.com>
2014-03-10 22:13:53 -0400

    jflte: restore pre-unification vzw home operator prop
    jfltevzw's default.prop set ro.cdma.home.operator.numeric to 311480
    jflte's unified init sets the prop to 310004 for phones formerly known as jfltevzw.
    
    Change-Id: I3c3a5e6c6783cc31c931b4699b9d2456a69b1e8e

--- init/init_jflte.c
+++ init/init_jflte.c
@@ -129,7 +129,7 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
         property_set("ro.telephony.default_cdma_sub", "0");
         property_set("ro.telephony.ril.v3", "newDriverCallU");
         property_set("ro.cdma.home.operator.alpha", "Verizon");
-        property_set("ro.cdma.home.operator.numeric", "310004");
+        property_set("ro.cdma.home.operator.numeric", "311480");
         property_set("telephony.lteOnCdmaDevice", "1");
         property_set("ro.telephony.default_network", "10");
     } else if (strstr(bootloader, "I9505G")) {
