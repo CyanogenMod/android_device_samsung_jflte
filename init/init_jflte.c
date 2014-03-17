058c4d0a6ac7febcf7b6bc91e6f3014133098670
Mike Kasick <mike@kasick.org>
2014-03-17 16:34:43 -0400

    init_jflte: cdma variants: Add "ro.gps.set_privacy=1" to enable/disable GPS privacy lock.
    Change-Id: Id67c8d1793de7de1a0da921d3fc2ffd3e9080385

--- init/init_jflte.c
+++ init/init_jflte.c
@@ -77,6 +77,7 @@ void vendor_load_properties()
         property_set("ro.telephony.ril.v3", "newDriverCallU");
         property_set("telephony.lteOnCdmaDevice", "1");
         property_set("ro.telephony.default_network", "8");
+        property_set("ro.gps.set_privacy", "1");
     } else if (strstr(bootloader, "R970X")) {
         /* jfltecsp */
         property_set("ro.build.fingerprint", "samsung/jfltecsp/jfltecsp:4.2.2/JDQ39/R970XWWUAMFA:user/release-keys");
@@ -88,6 +89,7 @@ void vendor_load_properties()
         property_set("telephony.lteOnCdmaDevice", "1");
         property_set("ro.telephony.default_network", "8");
         property_set("telephony.sms.pseudo_multipart", "1");
+        property_set("ro.gps.set_privacy", "1");
     } else if (strstr(bootloader, "L720")) {
         /* jfltespr */
         property_set("ro.build.fingerprint", "samsung/jfltespr/jfltespr:4.2.2/JDQ39/L720VPUAMDL:user/release-keys");
@@ -100,6 +102,7 @@ void vendor_load_properties()
         property_set("telephony.lteOnCdmaDevice", "1");
         property_set("ro.telephony.default_network", "8");
         property_set("telephony.sms.pseudo_multipart", "1");
+        property_set("ro.gps.set_privacy", "1");
     } else if (strstr(bootloader, "M919")) {
         /* jfltetmo */
         property_set("ro.build.fingerprint", "samsung/jfltetmo/jfltetmo:4.3/JSS15J/M919UVUEMK2:user/release-keys");
@@ -121,6 +124,7 @@ void vendor_load_properties()
         property_set("telephony.lteOnCdmaDevice", "1");
         property_set("ro.telephony.default_network", "8");
         property_set("telephony.sms.pseudo_multipart", "1");
+        property_set("ro.gps.set_privacy", "1");
     } else if (strstr(bootloader, "I545")) {
         /* jfltevzw */
         property_set("ro.build.fingerprint", "Verizon/jfltevzw/jfltevzw:4.3/JSS15J/I545VRUEMK2:user/release-keys");
@@ -133,6 +137,7 @@ void vendor_load_properties()
         property_set("ro.cdma.home.operator.numeric", "311480");
         property_set("telephony.lteOnCdmaDevice", "1");
         property_set("ro.telephony.default_network", "10");
+        property_set("ro.gps.set_privacy", "1");
     } else if (strstr(bootloader, "I9505G")) {
         /* jgedlte */
         property_set("ro.build.fingerprint", "samsung/jgedlteue/jgedlte:4.4.2/KOT49H.S001/131204:user/release-keys");
