72609b132140158db8e86da402cc245c39af9af6
Dan Pasanen <dan.pasanen@gmail.com>
2014-03-24 07:12:10 -0500

    add jflteusc to assert
    how did we miss this? =/
    
    Change-Id: Icf489ce050de49751e108c4340ed0d2afd5175ad

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -116,7 +116,7 @@ TARGET_ADD_ISO_MODE_1600 := true
 TARGET_ADD_ISO_MODE_HJR := true
 
 # Assert
-TARGET_OTA_ASSERT_DEVICE := jflte,jfltexx,i9505,GT-I9505,jgedlte,i9505g,GT-I9505G,jfltevzw,jfltespr,jfltetmo,jfltecri,jflteatt,jfltecan,jflteusc,jfltezm
+TARGET_OTA_ASSERT_DEVICE := jflte,jfltexx,i9505,GT-I9505,jgedlte,i9505g,GT-I9505G,jfltevzw,jfltespr,jfltetmo,jfltecri,jfltecsp,jflteatt,jfltecan,jflteusc,jfltezm
 
 # Kernel
 TARGET_KERNEL_CONFIG         := jf_eur_defconfig
