f38ea3b37b24f76b10ada78fdc4979917d99aa3d
invisiblek <dan.pasanen@gmail.com>
2014-06-19 07:36:56 -0500

    loki: update to latest code from djrbliss (v1.9)
    Change-Id: Ic3087935be45becb82990ae140fd949ac3284f41

--- releasetools/loki_flash.c
+++ releasetools/loki_flash.c
@@ -6,6 +6,7 @@
  * by Dan Rosenberg (@djrbliss)
  *
  */
+
 #include <stdio.h>
 #include <fcntl.h>
 #include <sys/mman.h>
@@ -13,63 +14,20 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include "loki.h"
 
-#define VERSION "1.2"
-
-#define BOOT_MAGIC_SIZE 8
-#define BOOT_NAME_SIZE 16
-#define BOOT_ARGS_SIZE 512
-
-struct boot_img_hdr
+int loki_flash(const char* partition_label, const char* loki_image)
 {
-	unsigned char magic[BOOT_MAGIC_SIZE];
-	unsigned kernel_size;	/* size in bytes */
-	unsigned kernel_addr;	/* physical load addr */
-	unsigned ramdisk_size;	/* size in bytes */
-	unsigned ramdisk_addr;	/* physical load addr */
-	unsigned second_size;	/* size in bytes */
-	unsigned second_addr;	/* physical load addr */
-	unsigned tags_addr;		/* physical addr for kernel tags */
-	unsigned page_size;		/* flash page size we assume */
-	unsigned dt_size;		/* device_tree in bytes */
-	unsigned unused;		/* future expansion: should be 0 */
-	unsigned char name[BOOT_NAME_SIZE];	/* asciiz product name */
-	unsigned char cmdline[BOOT_ARGS_SIZE];
-	unsigned id[8];			/* timestamp / checksum / sha1 / etc */
-};
-
-struct loki_hdr
-{
-	unsigned char magic[4];		/* 0x494b4f4c */
-	unsigned int recovery;		/* 0 = boot.img, 1 = recovery.img */
-	unsigned char build[128];	/* Build number */
-};
-
-#define PATTERN1 "\xf0\xb5\x8f\xb0\x06\x46\xf0\xf7"
-#define PATTERN2 "\xf0\xb5\x8f\xb0\x07\x46\xf0\xf7"
-#define PATTERN3 "\x2d\xe9\xf0\x41\x86\xb0\xf1\xf7"
-#define ABOOT_BASE 0x88dfffd8
-
-int main(int argc, char **argv)
-{
-
 	int ifd, aboot_fd, ofd, recovery, offs, match;
 	void *orig, *aboot, *patch;
 	struct stat st;
 	struct boot_img_hdr *hdr;
 	struct loki_hdr *loki_hdr;
-	char prop[256], outfile[1024], buf[4096];
+	char outfile[1024];
 
-	if (argc != 3) {
-		printf("[+] Usage: %s [boot|recovery] [in.lok]\n", argv[0]);
-		return 1;
-	}
-
-	printf("[+] loki_flash v%s\n", VERSION);
-
-	if (!strcmp(argv[1], "boot")) {
+	if (!strcmp(partition_label, "boot")) {
 		recovery = 0;
-	} else if (!strcmp(argv[1], "recovery")) {
+	} else if (!strcmp(partition_label, "recovery")) {
 		recovery = 1;
 	} else {
 		printf("[+] First argument must be \"boot\" or \"recovery\".\n");
@@ -77,15 +35,15 @@ int main(int argc, char **argv)
 	}
 
 	/* Verify input file */
-	aboot_fd = open("/dev/block/platform/msm_sdcc.1/by-name/aboot", O_RDONLY);
+	aboot_fd = open(ABOOT_PARTITION, O_RDONLY);
 	if (aboot_fd < 0) {
 		printf("[-] Failed to open aboot for reading.\n");
 		return 1;
 	}
 
-	ifd = open(argv[2], O_RDONLY);
+	ifd = open(loki_image, O_RDONLY);
 	if (ifd < 0) {
-		printf("[-] Failed to open %s for reading.\n", argv[2]);
+		printf("[-] Failed to open %s for reading.\n", loki_image);
 		return 1;
 	}
 
@@ -117,7 +75,7 @@ int main(int argc, char **argv)
 	}
 
 	/* Verify the to-be-patched address matches the known code pattern */
-	aboot = mmap(0, 0x10000, PROT_READ, MAP_PRIVATE, aboot_fd, 0);
+	aboot = mmap(0, 0x40000, PROT_READ, MAP_PRIVATE, aboot_fd, 0);
 	if (aboot == MAP_FAILED) {
 		printf("[-] Failed to mmap aboot.\n");
 		return 1;
@@ -127,11 +85,28 @@ int main(int argc, char **argv)
 
 	for (offs = 0; offs < 0x10; offs += 0x4) {
 
-		patch = hdr->ramdisk_addr - ABOOT_BASE + aboot + offs;
+		patch = NULL;
+
+		if (hdr->ramdisk_addr > ABOOT_BASE_LG)
+			patch = hdr->ramdisk_addr - ABOOT_BASE_LG + aboot + offs;
+		else if (hdr->ramdisk_addr > ABOOT_BASE_SAMSUNG)
+			patch = hdr->ramdisk_addr - ABOOT_BASE_SAMSUNG + aboot + offs;
+		else if (hdr->ramdisk_addr > ABOOT_BASE_VIPER)
+			patch = hdr->ramdisk_addr - ABOOT_BASE_VIPER + aboot + offs;
+		else if (hdr->ramdisk_addr > ABOOT_BASE_G2)
+			patch = hdr->ramdisk_addr - ABOOT_BASE_G2 + aboot + offs;
+
+		if (patch < aboot || patch > aboot + 0x40000 - 8) {
+			printf("[-] Invalid .lok file.\n");
+			return 1;
+		}
 
 		if (!memcmp(patch, PATTERN1, 8) ||
 			!memcmp(patch, PATTERN2, 8) ||
-			!memcmp(patch, PATTERN3, 8)) {
+			!memcmp(patch, PATTERN3, 8) ||
+			!memcmp(patch, PATTERN4, 8) ||
+			!memcmp(patch, PATTERN5, 8) ||
+			!memcmp(patch, PATTERN6, 8)) {
 
 			match = 1;
 			break;
@@ -146,8 +121,8 @@ int main(int argc, char **argv)
 	printf("[+] Loki validation passed, flashing image.\n");
 
 	snprintf(outfile, sizeof(outfile),
-			 "/dev/block/platform/msm_sdcc.1/by-name/%s",
-			 recovery ? "recovery" : "boot");
+			 "%s",
+			 recovery ? RECOVERY_PARTITION : BOOT_PARTITION);
 
 	ofd = open(outfile, O_WRONLY);
 	if (ofd < 0) {
