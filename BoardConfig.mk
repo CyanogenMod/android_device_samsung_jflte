35c1d8388d02aa2e1e7779f54621ac0049074f83
Dan Pasanen <dan.pasanen@gmail.com>
2014-07-25 18:30:00 -0500

    Revert: jf: fix race condition at boot
    Seems to be fixed to work properly now
    
    Change-Id: Ib577525ef1b1b6ef06872891aa1807df13f67878

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -39,7 +39,7 @@ TARGET_BOOTLOADER_BOARD_NAME := MSM8960
 TARGET_OTA_ASSERT_DEVICE := jflte
 
 # Kernel
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 zcache msm_rtb.filter=0x3F ehci-hcd.park=3 maxcpus=2
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 zcache msm_rtb.filter=0x3F ehci-hcd.park=3
 BOARD_KERNEL_BASE := 0x80200000
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
 BOARD_KERNEL_PAGESIZE := 2048
